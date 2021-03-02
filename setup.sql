88a7b13683b7ddb51830745b73f848f0904c88a1
Anthony Wong <antawck@gmail.com>
2021-03-02 09:00:44 +0000

    sql schema

---
+++ setup.sql
@@ -0,0 +1,25 @@
+SET autocommit = 0;
+
+CREATE TABLE members (
+    member varchar(30),
+    PRIMARY KEY (member)
+);
+
+INSERT INTO members VALUES
+("Nayeon"),
+("Jeongyeon"),
+("Momo"),
+("Sana"),
+("Jihyo"),
+("Mina"),
+("Dahyun"),
+("Chaeyoung"),
+("Tzuyu");
+
+CREATE TABLE rankings (
+    user varchar(255),
+    member varchar(30),
+    ranking int,
+    FOREIGN KEY (member) REFERENCES members(member)
+);
+
