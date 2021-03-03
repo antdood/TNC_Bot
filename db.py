84be81369ad69aaeba3e16d1b5bdad7eafca8bd6
Anthony Wong <antawck@gmail.com>
2021-03-03 06:21:18 +0000

    db functions streamlined

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 import os
 import MySQLdb
+from members import nickToName
 
 load_dotenv()
 
 class db:
+    """Highly customized db class for the purposes of this bot. Loads of very specific helper functions to wrap the mess of db handling away."""
+
     _instance = None
 
     def __init__(self):
@@ -25,8 +28,25 @@ class db:
         return
 
     @staticmethod
-    def getdb(self):
+    def getdb():
         if(db._instance == None):
             db()
 
-        return db._instance.cursor()
+        return db._instance
+
+    def newRankings(id, members):
+        c = db.getdb().cursor()
+        c.execute(f"DELETE FROM rankings WHERE user={id}")
+        
+        for rank, m in enumerate(members):
+            c.execute(f"INSERT INTO rankings VALUES({id}, '{nickToName(m)}', {rank+1})")
+
+        db.getdb().commit()
+
+        return
+
+    def getRankings(id):
+        c = db.getdb().cursor()
+        c.execute(f"SELECT * FROM rankings WHERE user={id} ORDER BY ranking")
+        return c.fetchall()
+
